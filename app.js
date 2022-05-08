f17932b8edeba7a3b9cc9bb835379cd728461282
Niroj925 <ngtc0925@gmail.com>
2022-05-08 07:34:07 +0545

    send mail through gmail api

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const path = require('path');
 const bodyParser= require('body-parser');
 const viewpath= path.join(__dirname, './view');
-var nodemailer=require('nodemailer');
+const nodemailer =require('nodemailer');
+const {google}=require('googleapis');
+
 const PORT=process.env.PORT||2200;
 
 require('dotenv').config();
@@ -28,7 +30,9 @@ console.log('mail:'+mail);
 console.log('btn val:'+btnval);
 
 if(btnval==='ok'){
-   send_mail(email,mail);
+  sendMail(email,mail)
+.then((result)=>console.log('email sent...',result))
+.catch((err)=>console.log(err.message));
   res.redirect('/');
 }
 })
@@ -41,28 +45,45 @@ if(btnval==='ok'){
 
 //lets make a function to send email 
 
-var send_mail=function(email,msg){
-  console.log('hello world');
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'hamroghar531@gmail.com',
-      pass: process.env.pass,
-    }
-  });
+const client_id='152987635030-v7gksg0ohvacpd1psvpb2io51bsldskn.apps.googleusercontent.com';
+const client_secret='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';
+const redirect_url='https://developers.google.com/oauthplayground';
+const refresh_token=
+'1//04VRP4qHQO1g9CgYIARAAGAQSNwF-L9Irx3Hqk1OMQ152PpkftHxBYABQL7RpkREtn80OIRSvfgHd1KJswV_0RLcSSHhqLfiaqZs';
 
-  var mailOptions = {
-    from: 'hamroghar531@gmail.com',
-    to: email,
-    subject: 'Send from NeoTech',
-    text: msg
-  };
+const oAuth2client=new google.auth.OAuth2(client_id,client_secret,redirect_url)
+oAuth2client.setCredentials({refresh_token:refresh_token});
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
+async function sendMail(email,msg){
+    try{
+        const accessToken=await oAuth2client.getAccessToken();
+        console.log(accessToken);
+       console.log(accessToken.token);
+        const transport=nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+                type:'OAuth2',
+                user:'hamroghar531@gmail.com',
+                clientId:client_id,
+                clientSecret:client_secret,
+                refershToken:refresh_token,
+                accessToken:accessToken.token,
+               // accessToken:'ya29.A0ARrdaM90xXpIWespBlASaiidy_zNu-LQy2TZjKeXhUodMPWpCW34efe2JE9U0XAgjRiz92A4AK5g91Erv8o6seKcj6817ZOL2mk4e7M_Sud7E4LuBn3j9HmS_Y4jxTxyq7bg_W7lhgMwS2Gv3LSM_h9iCfnCEA'
+            }
+        })
+
+        const mailOptions={
+            from:'hamroghar531@gmail.com',
+            to:email,
+            subject:'email from gmail api',
+            text:msg
+        };
+        const result=await transport.sendMail(mailOptions);
+        return result;
+
+    }catch(err){
+        return err;
     }
-  });
+
 }
+
